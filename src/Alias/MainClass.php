779cb89361ff5ae8516d270538004ff5ff9259ef
jazzwhistle <jdcraftmcpe@gmail.com>
2017-12-11 00:17:38 +0100

    Reverted to original version, allow alias on offline players, updates

--- src/Alias/MainClass.php
+++ src/Alias/MainClass.php
@@ -12,9 +12,13 @@ use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
 class MainClass extends PluginBase implements Listener{
-    //TODO get 'LastIp' from simpleauth
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $simpleauth = $this->getServer()->getPluginManager()->getPlugin("SimpleAuth");
+        if($simpleauth == null){
+            $this->getLogger()->info(TextFormat::YELLOW."You do not have SimpleAuth");
+			$this->getLogger()->info(TextFormat::YELLOW."You can only get a players alias if he/she is online!");
+		}
     }
     public function onDisable(){}
     public function onJoin(PlayerJoinEvent $event){
@@ -27,7 +31,7 @@ class MainClass extends PluginBase implements Listener{
         $file->set($name);
         $file->save();
     }
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args):bool {
         switch($command->getName()){
             case "alias":
                 if(!isset($args[0])){
@@ -41,12 +45,28 @@ class MainClass extends PluginBase implements Listener{
                     $file = new Config($this->getDataFolder() . "players/".$ip.".txt");
                     $names = $file->getAll(true);
                     $names = implode(', ', $names);
-                    $sender->sendMessage(TextFormat::GREEN."[Alias] Showing alias of ".$name."...");
-                    $sender->sendMessage(TextFormat::BLUE."[Alias] ".$names."");
+                    $sender->sendMessage(TextFormat::GREEN."[Alias] Showing players who joined from the same IP as ".$name."...");
+                    $sender->sendMessage(TextFormat::AQUA. $names);
                     return true;
+                    break;
                 }else{
-                    $sender->sendMessage(TextFormat::YELLOW."Player does not exist");
-                    return true;
+                    $simpleauth = $this->getServer()->getPluginManager()->getPlugin("SimpleAuth");
+                    if($simpleauth !== null){
+                        $saconfig = $simpleauth->getDataProvider()->getPlayerData($name);
+                        if ($saconfig !== null && isset($saconfig['ip']) && strlen($saconfig['ip']) > 0){
+                        $lastip = $saconfig['ip'];
+                        $file = new Config($this->getDataFolder()."players/".$lastip.".txt");
+                        $names = $file->getAll(true);
+                        $names = implode(', ', $names);
+                        $sender->sendMessage(TextFormat::GREEN."[Alias] Showing players who joined from the same IP as ".$name."...");
+                        $sender->sendMessage(TextFormat::AQUA. $names."");
+                        return true;
+						}else{
+						$sender->sendMessage(TextFormat::RED."Player not found");
+						}
+                    }else{
+                        $sender->sendMessage(TextFormat::RED."SimpleAuth is not enabled, the player must be online");
+                    }
                 }
                 return true;
         }
